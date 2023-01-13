613d6de6668f745b3cd94bebd89e3d8424b2f1ad
hcjplj <42893746+hcjplj@users.noreply.github.com>
2023-01-14 03:59:42 +0900

    Create 기능 추가

--- main.dart
+++ main.dart
@@ -1,16 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -19,16 +22,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: (){
-
-                  },
-                  child: Text('+')
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                      //Create, Update
+                        final sample = { "age":20, "name":"jone"};
+                        final a = FirebaseFirestore.instance.collection('tea').doc('num');
+                        await a.set (sample);
+                      },
+                      child: Text('+'),
               ),
             ],
           ),
