2095f0733a4c7675b977cddcadc4ab5d2ff7edac
hcjplj <42893746+hcjplj@users.noreply.github.com>
2023-01-14 04:06:27 +0900

    delete 기능 추가

--- main.dart
+++ main.dart
@@ -4,40 +4,54 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+    runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                      //Create, Update
-                        final sample = { "age":20, "name":"jone"};
-                        final a = FirebaseFirestore.instance.collection('tea').doc('num');
-                        await a.set (sample);
-                      },
-                      child: Text('+'),
-              ),
-            ],
-          ),
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final db = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () async {
+                //Create, Update
+                final sample = { "서명":"플러터기초", "가격":40000, };
+                final a = db.collection('책').doc('IT도서');
+                await a.set(sample);
+              },
+              child: Text('+'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                //Delete
+                final aa = db.collection('책').doc('IT도서');
+                final sample = <String, dynamic>{ "가격":FieldValue.delete()};
+                await aa.update(sample);
+              },
+              child: Text('-'),
+            ),
+          ],
         ),
       ),
     );
